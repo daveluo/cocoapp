3e46714cbe055f5dbde201f6fa528b5b5e0e7b54
paperspace <dluo.mssm@gmail.com>
2018-05-01 18:17:35 -0400

    changed port

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # -*- encoding: utf-8 -*-
+
 from cocoapp import app
 from cocoapp.model2 import *
 
 if __name__ == "__main__":
-    # app.run()
-    app.run(host='0.0.0.0', debug=True, port=8889)
+    #app.run()
+    import cocoapp.model2
+    app.run(host="0.0.0.0", debug=True, port=5000)
