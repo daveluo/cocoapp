9fb531b6caf5988650b0d753f40d026debd25da8
Dave <dluo.mssm@gmail.com>
2018-05-07 09:40:26 -0700

    css active, moved stuff to app.js

--- cocoapp/static/js/app.js
+++ cocoapp/static/js/app.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    console.log('test')
     var $status = $('.status');
     var results = {
         bboxes: []
     };
 
-   var drawBBox = function() {
+    var drawBBox = function() {
         var threshold = parseInt($("#bbox-slider").text());
         var c = document.getElementById("img-canvas");
         var ctx = c.getContext("2d");
@@ -34,18 +35,9 @@ $(document).ready(function() {
         });
     };
 
-    // With JQuery
-    $('#bbox-slider').slider({
-        formatter: function(value) {
-            return value;
-        }
-    })
-    .on('slide', drawBBox)
-    .data('slider');
-
     $('#img').change(function(event) {
         var obj = $(this)[0];
-
+        // console.log(obj.files, obj.files[0])
         $status.html('');
 
         if (obj.files && obj.files[0]) {
@@ -73,11 +65,11 @@ $(document).ready(function() {
         }
 
         var imageData = new FormData($(this)[0]);
-
+        console.log($(this)[0]);
         $status.html(
             `<span class='eval'>Evaluating...</span>`
         );
-
+        
         $.ajax({
             url: '/predict',
             type: 'POST',
@@ -110,7 +102,85 @@ $(document).ready(function() {
         });
     });
 
+    $(".sample_img").click(function() {
+         
+        // add active class to clicked picture
+        $(".sample_img").removeClass("active");
+        $(this).addClass("active");
 
+     
+        // grab image url
+        var url = $(this).attr("src");
+
+        // read url into blob using XHR
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'blob';
+        
+        request.onload = function() {
+            var reader = new FileReader();
+            reader.readAsDataURL(request.response);
+
+            // draw canvas of selected sample image
+            reader.onload =  function(e){
+                // console.log('DataURL:', e.target.result);
+                $('.img-hidden').html(
+                    `<img id='loaded-img' src='${e.target.result}'/>`
+                    );
+                var c = document.getElementById("img-canvas");
+                var ctx = c.getContext("2d");
+                var img = document.getElementById("loaded-img");
+                img.addEventListener("load", function(e) {
+                    ctx.drawImage(img,0,0, 500, 500);
+                });
+
+                // blob into form data
+                var blob = request.response;
+                var fd = new FormData();
+                fd.set('file', blob);
+                // console.log(formD);
+
+                $.ajax({
+                    url: '/predict',
+                    type: 'POST',
+                    processData: false,
+                    contentType: false,
+                    dataType: 'json',
+                    data: fd,
+
+                    success: function(responseData) {
+                        if (responseData.error === 'bad-type') {
+                            console.log('no good')
+                            $status.html(
+                                `<span class='eval'>Valid file types are .jpg and .png</span>`
+                            );
+                        } else {
+                            results["bboxes"] = responseData["bboxes"];
+                            let preData = JSON.stringify(responseData, null, '\t');
+                            $status.html(
+                                `<span class='result success'>Results</span>
+                                 <pre>${preData}</pre>`
+                            );
+                            // Draw Bounding boxes
+                            drawBBox();
+                         }
+                    },
+                    error: function() {
+                        $status.html(
+                            `<span class='eval'>Something went wrong, try again later.</span>`
+                        );
+                    }
+                });
+            };
+        };
+        request.send();
+    });   
     
-
+    $('#bbox-slider').slider({
+        formatter: function(value) {
+            return value;
+        }
+    })
+    .on('slide', drawBBox)
+    .data('slider');
 });
